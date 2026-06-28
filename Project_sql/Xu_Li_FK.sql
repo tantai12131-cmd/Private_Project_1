c344d68d19ac09e7d6ce80341cec950fbd26e028
VuTanTai <tantai12131@gmail.com>
2026-06-28 21:00:37 +0700

    Revenue_Problem

---
+++ Project_sql/Xu_Li_FK.sql
@@ -0,0 +1,12 @@
+
+--Create FK: items.sku = recipes.recipe_id
+ALTER TABLE items
+ADD CONSTRAINT uq_items_sku UNIQUE (sku);
+
+ALTER TABLE recipes
+ADD CONSTRAINT fk_recipes_items
+FOREIGN KEY (recipe_id)
+REFERENCES items(sku);
+
+
+
